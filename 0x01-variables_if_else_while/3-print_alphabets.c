f51428ac5cd4c3e1ca5a46b2128b9c9f94602f46
henapartae123 <henyvibe@gmail.com>
2022-03-13 05:43:25 -0700

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - header
+ * Description: put alphabet in lowercase and uppercase
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+return (0);
+}
