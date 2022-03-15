25469991c4fd5d19e545c3dced529922edd508e1
henapartae123 <henyvibe@gmail.com>
2022-03-15 10:37:55 -0700

    c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - block
+ * Description: check for alphabetic character
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
+ */
+int main(void)
+{
+	char c;
+
+	if (isalpha(c) == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
