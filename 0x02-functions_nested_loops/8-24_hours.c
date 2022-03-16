91d40548e7085ac32d19a2f8a83a85021bdf4ad8
henapartae123 <henyvibe@gmail.com>
2022-03-16 00:19:21 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,7 +20,7 @@ void jack_bauer(void)
 		while (hours_ones < hours_max)
 		{
 			minutes_tens = '0';
-			while (miinutes_tens < '6')
+			while (minutes_tens < '6')
 			{
 				minutes_ones = '0';
 				while (minutes_ones < 58)
