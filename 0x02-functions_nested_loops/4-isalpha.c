5533aa3b0e3667fafb5e4499f59884626023b2c3
henapartae123 <henyvibe@gmail.com>
2022-03-15 23:42:48 -0700

    c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,9 @@
  * Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
  */
 int _isalpha(int c)
-{
-	char c;
 
-	if (isalpha(c) == 0)
+
+	if (_isalpha(c) == 0)
 	{
 		return (1);
 	}
