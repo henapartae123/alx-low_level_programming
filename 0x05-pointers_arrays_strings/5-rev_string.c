f554240c49298ec7ea8d922cd9b219dcbd03fa3a
henapartae123 <henyvibe@gmail.com>
2022-03-24 04:19:44 -0700

    revstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i, c, l;
+	char h;
+
+	for (i = 0; s[i] != '\0'; i++);
+		;
+	l = i;
+	for (i--, c = 0; c < 1 / 2; i--, c++)
+	{
+		h = s[c];
+		s[c] = s[i];
+		s[i] = h;
+	}
+}
