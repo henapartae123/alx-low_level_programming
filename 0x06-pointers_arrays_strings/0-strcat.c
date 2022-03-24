6135988777985bdb1714533e7a0cb4a993f52ec0
henapartae123 <henyvibe@gmail.com>
2022-03-24 08:05:46 -0700

    c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @src: string to append
+ * @dest: string to be appended
+ * Return dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] !='\0'; j++)
+	{
+		dest[i + J] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
