dd1e8c9e20e278de8d8c71feb590b71d02803683
henapartae123 <henyvibe@gmail.com>
2022-04-08 00:55:43 -0700

    range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,7 +23,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0; index < size; indez++)
+	for (index = 0; index < size; index++)
 		array[index] = min++;
 
 	return (array);
