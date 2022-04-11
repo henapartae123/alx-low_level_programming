56577a3357d4ab1cbdd0305274371c4737aa6f31
henapartae123 <henyvibe@gmail.com>
2022-04-11 05:26:40 -0700

    doggo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,14 +12,14 @@ typedef struct dog
 	float age;
 	char *owner;
 }
-doggo;
+dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-doggo *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
-void free_dog(doggo *d);
+void free_dog(dog_t *d);
 
 #endif /* _dog_h_ */
