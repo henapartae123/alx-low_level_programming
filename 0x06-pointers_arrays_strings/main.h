1b9805835a0fe2ec4a20b6215ef8054c4e506ab9
henapaartae123 <henyvibe@gmail.com>
2022-04-26 10:56:36 +0300

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *s);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _putchar(char c);
 void print_buffer(char *b, int size);
