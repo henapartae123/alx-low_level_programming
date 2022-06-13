ad653cbc39c91634f0087c1f4734567c97defe53
henapaartae123 <henyvibe@gmail.com>
2022-06-13 12:43:00 +0300

    dyn

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * *_strchr - locates a character in a string
--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * *_strpbrk - searches a string for any of a set of bytes
