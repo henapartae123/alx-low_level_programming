53cab0ec89a8115a9a027ca0f98ed13332f3fd76
unknown <henyvibe@gmail.com>
2022-12-11 10:51:40 +0300

    get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,20 +8,23 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 
-hash_node_t *node;
-unsigned long int index;
+	hash_node_t *tmp;
+	unsigned long int idx;
 
-if (ht == NULL || key == NULL)
-	return (NULL);
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-index = key_index((const unsigned char *) key, ht->size);
-node = ht->array[index];
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
 
-while (node != NULL)
-{
-	if (strcmp(node->key, key) == 0)
-		return (node->value);
+	if (tmp == NULL)
+		return (NULL);
 
-	node = node->next;
+	while (strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+
+	if (tmp == NULL)
+		return (NULL);
+
+	return (tmp->value);
 }
-return (NULL);
