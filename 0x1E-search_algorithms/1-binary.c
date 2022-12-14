97728b568af033511ff32b229346b70554759791
unknown <henyvibe@gmail.com>
2022-12-14 17:19:45 +0300

    linear

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,37 +9,36 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-	ssize_t mid = size / 2;
-	size_t low = 0;
-	size_t high = size - 1;
+    size_t i = 0;
+    ssize_t mid = size / 2;
+    size_t low = 0;
+    size_t high = size - 1;
 
-	if (array == NULL)
-		return (-1);
-	while (1)
-	{
-		printf("Searching in array: %d", array[low]);
-		for (i = low + 1; i <= high; i++)
-		{
-			printf(", %d", array[i]);
-		}
-		printf("\n");
-		mid = low + (high - low) / 2;
-		if (array[mid] == value)
-			return (mid);
-		if (array[mid] < value)
-		{
-			low = mid + 1;
-			high = high;
-
-		}
-		else
-		{
-			high = mid - 1;
-			low = low;
-		}
-		if (low > high)
-			break;
-	}
-	return (-1);
+    if (array == NULL)
+        return (-1);
+    while (1)
+    {
+        printf("Searching in array: %d", array[low]);
+        for (i = low + 1; i <= high; i++)
+        {
+            printf(", %d", array[i]);
+        }
+        printf("\n");
+        mid = low + (high - low) / 2;
+        if (array[mid] == value)
+            return (mid);
+        if (array[mid] < value)
+        {
+            low = mid + 1;
+            high = high;
+        }
+        else
+        {
+            high = mid - 1;
+            low = low;
+        }
+        if (low > high)
+            break;
+    }
+    return (-1);
 }
--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,28 +9,28 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t start, next = 0, i;
+    size_t start, next = 0, i;
 
-	if (array == NULL)
-		return (-1);
-	printf("Value checked array[%ld] = [%d]\n", next, array[next]);
-	next = floor(sqrt(size));
-	while (array[next] < value)
-	{
-		printf("Value checked array[%ld] = [%d]\n", next, array[next]);
-		next = next + floor(sqrt(size));
-		if (next >= size)
-			break;
-	}
-	start = next - floor(sqrt(size));
-	printf("Value found between indexes [%ld] and [%ld]\n", start, next);
-	if (next >= size)
-		next = size - 1;
-	for (i = start; i <= next; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
+    if (array == NULL)
+        return (-1);
+    printf("Value checked array[%ld] = [%d]\n", next, array[next]);
+    next = floor(sqrt(size));
+    while (array[next] < value)
+    {
+        printf("Value checked array[%ld] = [%d]\n", next, array[next]);
+        next = next + floor(sqrt(size));
+        if (next >= size)
+            break;
+    }
+    start = next - floor(sqrt(size));
+    printf("Value found between indexes [%ld] and [%ld]\n", start, next);
+    if (next >= size)
+        next = size - 1;
+    for (i = start; i <= next; i++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+    return (-1);
 }
